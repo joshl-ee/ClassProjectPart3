f3383b9529d59d11948ccb2bd87c9585b873ce71
Josh Lee <joshlee@usc.edu>
2023-04-08 19:58:46 -0700

    Part 2 Check

---
+++ src/CSCI485ClassProject/DBConf.java
@@ -0,0 +1,7 @@
+package CSCI485ClassProject;
+
+public class DBConf {
+  public static int FDB_API_VERSION = 710;
+  public static String TABLE_SCHEMA_STORE = "schema";
+  public static String TABLE_RECORDS_STORE = "records";
+}
