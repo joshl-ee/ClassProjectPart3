ba3323a4784141933097b8105c1a77dfd48eb3c6
Josh Lee <joshlee@usc.edu>
2023-04-11 10:25:53 -0700

    here

--- src/CSCI485ClassProject/Cursor.java
+++ src/CSCI485ClassProject/Cursor.java
@@ -276,7 +276,7 @@ public class Cursor {
 
       // Get primary key
       pkValTuple = keyTuple.popFront();
-
+      System.out.println("Looking for PK " + pkValTuple.get(0).toString() + "in main");
       newIterator = FDBHelper.getKVPairIterableStartWithPrefixInDirectory(directorySubspace, tx, pkValTuple, isInitializedToLast).iterator();
     }
 
